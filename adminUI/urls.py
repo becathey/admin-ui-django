0bf29c9dfab82ad1c96b7833c1feb3be771fe07e
Bill Cathey <billcathey@hotmail.com>
2023-02-01 22:49:55 -0500

    Update secret key config

--- adminUI/urls.py
+++ adminUI/urls.py
@@ -9,6 +9,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(routers.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
